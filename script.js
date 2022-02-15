3fb806b1676c51ec13a1cb16566392bb405c6cdd
Henrique belard pignaton <38145467+henrique-pignaton@users.noreply.github.com>
2022-02-15 16:10:22 -0300

    kok

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function henrique(){    
+function henrique() {
     let div = document.getElementById('primeira-foto')
 
-    div.innerHTML = '<div class="row justify-content-center opa" id="cabeçalho"> <div class="col-2 perfil-foto"> <img src="henrique-perfil.JPG" alt="" class="perfil-foto"> </div> <div class="col-8 nome-user"> <h3 class="nome-user">Henrique Pignaton</h3> <h5 class="cidade">Sitio Posada - Santa Tereza, ES</h5> </div> <div class="col-1 tres-bolinhas"> <h2>...</h2> </div> </div> <div class="fed foto-fed"> <img src="image3.jpeg" width="100%" height="100%" alt=""> <div class="row"> <h6 style="margin-bottom: 0px;">90 likes</h6> <div class="col-1"><i class="bi bi-heart"></i></div> <div class="col-1"><i class="bi bi-chat"></i></div> <div class="col-1"><i class="bi bi-send"></i></div> </div> </div>'
+    div.innerHTML = '<div class="row justify-content-center opa"id=cabeçalho><div class="perfil-foto col-2"><img alt=""src=henrique-perfil.JPG class=perfil-foto></div><div class="nome-user col-8"><h3 class=nome-user>Henrique Pignaton</h3><h5 class=cidade>Sitio Posada - Santa Tereza, ES</h5></div><div class="col-1 tres-bolinhas"><h2>...</h2></div></div><div class="fed foto-fed"><img alt=""src=image3.jpeg height=100% width=100%><div class=row><div class=col-1><i class="bi bi-heart"></i></div><div class=col-1><i class="bi bi-chat"></i></div><div class=col-1><i class="bi bi-send"></i></div><h6 style=margin-bottom:0>Curtido por sdjfkak.ppo2 e outras 140 pessoas</h6></div></div>'
 
 }
 
 
 
-function heartred(){
+function heartred() {
     let input = document.getElementById('core')
-    input.style.color = 'red'    
+    input.style.color = 'red'
 }
 
 

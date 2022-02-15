a512a04938aa63bde7cda8c0ed98ea914c1f852c
Henrique belard pignaton <38145467+henrique-pignaton@users.noreply.github.com>
2022-02-15 11:30:02 -0300

    fase de teste com Java Script

---
+++ script.js
@@ -0,0 +1,4 @@
+function cabecalho(){
+    let div = document.getElementById('primeira-foto')
+    div.innerHTML = '<div class="fed" id="primeira-foto"> <div class="row justify-content-center opa" id="cabeçalho"> <div class="col-2 perfil-foto"> <img src="henrique-perfil.JPG" alt="" class="perfil-foto"> </div> <div class="col-8 nome-user"> <h3 class="nome-user">Henrique Pignaton</h3> <h5 class="cidade">Sitio Posada - Santa Tereza, ES</h5> </div> <div class="col-1 tres-bolinhas"> <h2>...</h2> </div> </div> </div>'
+}

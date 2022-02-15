afcc62c2d3f9f15e5fe6f312f0d7ec26a296c09b
Henrique belard pignaton <38145467+henrique-pignaton@users.noreply.github.com>
2022-02-15 13:06:13 -0300

    coração like red

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function henrique(){    
     let div = document.getElementById('primeira-foto')
+
     div.innerHTML = '<div class="row justify-content-center opa" id="cabeçalho"> <div class="col-2 perfil-foto"> <img src="henrique-perfil.JPG" alt="" class="perfil-foto"> </div> <div class="col-8 nome-user"> <h3 class="nome-user">Henrique Pignaton</h3> <h5 class="cidade">Sitio Posada - Santa Tereza, ES</h5> </div> <div class="col-1 tres-bolinhas"> <h2>...</h2> </div> </div> <div class="fed foto-fed"> <img src="image3.jpeg" width="100%" height="100%" alt=""> <div class="row"> <h6 style="margin-bottom: 0px;">90 likes</h6> <div class="col-1"><i class="bi bi-heart"></i></div> <div class="col-1"><i class="bi bi-chat"></i></div> <div class="col-1"><i class="bi bi-send"></i></div> </div> </div>'
 
 }
 
 
 
+function heartred(){
+    let input = document.getElementById('core')
+    input.style.color = 'red'    
+}
 
 
